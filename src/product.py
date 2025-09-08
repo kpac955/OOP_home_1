d761fe53a2da46d8655b229195aeb6694f0de07d
kotbk <kpas95545@gmail.com>
2025-09-08 23:30:14 +0400

    complite category.py and product.py, file README.md

---
+++ src/product.py
@@ -0,0 +1,24 @@
+
+
+class Product:
+    name: str
+    description: str
+    price: float
+    quantity: int
+    product_count = 0
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+if __name__ == "__main__":
+    product1 = Product('Samsung Galaxy S21', 'Cмартфон на android', '32000', '100')
+    product2 = Product('Iphone 11', 'Смартфон на IOS', '44000', '67')
+
+    print(product1.name)
+    print(product1.price)
+
+    print(product2.name)
+    print(product2.price)
