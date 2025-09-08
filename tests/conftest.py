4f7760dbcfb1935ccc83d7936453fbc8bbc33ffd
kotbk <kpas95545@gmail.com>
2025-09-09 00:38:02 +0400

    complite test_category.py and html.cov

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,18 +11,7 @@ def first_category():
         description = "Телефоны на операционной системе Android",
         products = [
             Product("Samsung Galaxy S21", "128GB, Фиолетовый цвет, 64MP камера", 42000.0, 5),
-            Product("Iphone 11", "128GB, Белый цвет, 64MP камера", 45000.0, 7)
-        ]
-    )
-
-@pytest.fixture
-def second_category():
-    return Category(
-        name = "Телевизоры",
-        description = "Телевизоры 4K",
-        products = [
-            Product("Samsung LXN00055", "4К, Wi-fi, 3HDMI d55 ", 55000.0, 8),
-            Product("LG XU2345N", "4K, Wi-fi, USB port, LAN port", 53000.0, 7)
+            Product("Samsung Galaxy A18", "128GB, Белый цвет, 64MP камера", 45000.0, 7)
         ]
     )
 
---
+++ tests/test_category.py
@@ -0,0 +1,7 @@
+def test_first_category(first_category):
+    assert first_category.name == "Cмартфоны"
+    assert first_category.description == "Телефоны на операционной системе Android"
+    assert len(first_category.products) == 2
+
+    assert first_category.category_count == 1
+    assert  first_category.product_count == 2
