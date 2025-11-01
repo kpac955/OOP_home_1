c6c57e99ca0eb59d97f64fab8974009f05de0d2e
kotbk <kpas95545@gmail.com>
2025-11-01 21:18:20 +0400

    реализован __add__ для product.py

--- src/product.py
+++ src/product.py
@@ -14,6 +14,12 @@ class Product:
     def __str__(self):
         return f'{self.name}, {self.price} руб. Остаток: {self.quantity} шт.'
 
+    def __add__(self, other):
+        if not isinstance(other, Product):
+            return
+        return (self.price * self.quantity) + (other.price * other.quantity)
+
+
     @property
     def price(self):
         return self.__price
