46b55a0f288f2e537970fc172beec912c122de71
kotbk <kpas95545@gmail.com>
2025-11-01 21:29:46 +0400

    реализованы тесты __add__ и __str__ для product.py

--- tests/test_product.py
+++ tests/test_product.py
@@ -50,3 +50,23 @@ def test_new_product_creation():
     assert new_product.description == "Бюджетный смартфон"
     assert new_product.price == 25000.0
     assert new_product.quantity == 7
+
+
+def test_str_product(product_1, product_2):
+    assert "Samsung Galaxy S21" in str(product_1)
+    assert "42000.0" in str(product_1)
+    assert "3" in str(product_1)
+    assert "руб." in str(product_1)
+    assert "Остаток:" in str(product_1)
+    assert "шт." in str(product_1)
+
+def test_add_product(product_1, product_2):
+    result = product_1 + product_2
+
+    # Проверка на то, что результат - число
+    assert isinstance(result, (int, float))
+
+    # Проверяем правильность вычислений
+    expected = (42000.0 * 3) + (44000.0 * 5)
+    assert result == expected
+    assert result == 346000.0
